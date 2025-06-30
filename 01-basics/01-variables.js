23df9d7b6c6b72f33a5c50367e0edba3fa386a9b
PranitPatil07 <pranit.patil@neosoftmail.com>
2025-06-30 08:31:15 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+let accountId=1234;
+const accountEmail="test@gmail.com";
+var accountPassword="test@123";
+accountCity=123;
+
+// accountEmail="hi@gmail.com"
+// accountId=12321
+// accountPassword=11111111;
+
+/*accountEmail="hi@gmail.com"
+accountId=12321
+accountPassword=11111111;
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
