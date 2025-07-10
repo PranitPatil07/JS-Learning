1240d017ad27d11ce1496feaa92ca2ec21e9ff7f
PranitPatil07 <pranit.patil@neosoftmail.com>
2025-07-10 12:18:26 +0000

    string done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+const name="Pranit";
+const count=50;
+
+// console.log(name+count+" value");
+
+console.log(`My name is ${name} count is ${count}`); //Right Way to do
+
+const gameName= new String('Pranit');
+console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));//a
+console.log(gameName.indexOf('p'));//-1 beacuse smallp does not their in gamename string
+console.log(gameName.substring(0,3));//Pra
+
